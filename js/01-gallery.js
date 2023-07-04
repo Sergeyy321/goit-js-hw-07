4ef49065d213e2d627c91ebe5f9c3d789f9fb471
Sergeyy321 <125265794+Sergeyy321@users.noreply.github.com>
2023-07-05 00:34:13 +0200

    commit-two

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryEl = document.querySelector('.gallery')
+
+function createMarkup(el) {
+return el.map(({preview,original,description})=> `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+   </a>
+  </li>`).join('')  
+}
+galleryEl.insertAdjacentHTML('beforeend', createMarkup(galleryItems))
+galleryEl.addEventListener('click', onClick)
+
+function onClick(el) {
+    el.preventDefault()
+    if (el.target.classList.contains('gallery__image')) {
+     let currentEl = el.target.dataset
+     if (currentEl){
+       const instance = basicLightbox.create(
+        `<img src="${event.target.dataset.source}"   width="1200" height="800">`)
+        instance.show()  
+     }      
+    }
+}
+
 console.log(galleryItems);
+
